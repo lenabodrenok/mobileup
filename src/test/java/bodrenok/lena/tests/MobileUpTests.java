26080fd8ef372f4e3426da46f76abfb14e259025
Elena <lena.bodrenok@gmail.com>
2022-05-17 23:53:29 +0300

    add vacancyTest

--- src/test/java/bodrenok/lena/tests/MobileUpTests.java
+++ src/test/java/bodrenok/lena/tests/MobileUpTests.java
@@ -7,12 +7,16 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 public class MobileUpTests {
@@ -31,12 +35,22 @@ public class MobileUpTests {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        step("Open " + baseUrl, () -> {
-            Selenide.open(baseUrl);
-        });
+        step("Open " + baseUrl, () -> Selenide.open(baseUrl));
         step("Check item: " + testData.rusName, () -> {
             $(".main-nav__list").$(byText(testData.rusName)).click();
             $(".grid").shouldBe(visible);
+        });
+    }
+
+    @DisplayName("Checking vacancy QA automation engineer")
+    @Test
+    void vacancyTest() {
+        step("Open " + baseUrl, () -> Selenide.open(baseUrl));
+        step("Select 'Вакансии'", () -> {
+            $$(".main-nav__link").findBy(text("Вакансии")).click();
+        });
+        step("Check 'QA auto'", () -> {
+            $$(".vacancies-front__vacancy-item").findBy(text("QA auto")).shouldBe(visible);
         });
     }
 }
