a64fc1830ebbb90da4ec67ec147a0c7b9195d6d1
Lena Bodrenok <lena.bodrenok@gmail.com>
2022-05-24 14:18:37 +0300

    Update CredentialsConfig.java

--- src/test/java/bodrenok/lena/config/CredentialsConfig.java
+++ src/test/java/bodrenok/lena/config/CredentialsConfig.java
@@ -1,1 +1,9 @@
+package bodrenok.lena.config;
 
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/credentials.properties")
+public interface CredentialsConfig extends Config {
+    String login();
+    String password();
+}
